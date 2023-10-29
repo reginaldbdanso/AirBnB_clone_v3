9a905a5fd9e4024df97c71f773b6708aa0193e03
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-10-29 23:14:47 +0000

    Updated api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,22 @@
 """ Index module """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats')
+def stats():
+    stats = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats)
