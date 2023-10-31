870b49e38fa1c341982552937dd6534205086215
reginaldbdanso <123961962+reginaldbdanso@users.noreply.github.com>
2023-10-31 19:25:58 +0000

    Updated api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Flask application """
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,13 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """ Close storage """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """ Handle 404 errors """
+    return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
